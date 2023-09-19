8d0bf1433d7dd3dd2056454caffcab8218ca1d57
masan894 <MasanaMail762@gmail.com>
2023-09-19 17:20:36 +0900

    k-group anon completed
    ログ保存とあとから入室した人へのログ提供が課題

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("login", (name) => {
     console.log(`${name} connected`);
-    io.emit("login", name);
     let roomNum = 1 + Math.floor(Math.random() * 8);
-    socket.join(roomNum);
-    socket.emit("roomNumSet", roomNum);
+    socket.join(roomNum); //内部の入室処理
+    socket.emit("roomNumSet", roomNum); //クライアント自身の画面にroomNumを表示させる
+    io.to(roomNum).emit("login", name); //部屋のメンバーに入室を通知
+    io.emit("addMember", { name, roomNum }); //メンバーリストにメンバーを追加
 
     socket.on("chat message", (msg) => {
-      io.to(roomNum).emit("chat message", { name, msg });
+      io.to(roomNum).emit("chat message", { name, msg, roomNum }); //ルームチャットに送信
+      io.emit("log message", { msg, roomNum }); //全体チャットに送信
     });
   });
 
