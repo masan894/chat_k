c2e458bb3587028106dc0aeadd2b0bbb501b1ad2
masan894 <MasanaMail762@gmail.com>
2023-10-12 14:51:22 +0900

    ログアウト+初回判定

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const Post = mongoose.model("Post", postSchema);
 
 // 保存するデータの形を定義する（データの種類が複数ある場合はそれぞれ１つずつ定義する）
 const nameSchema = new mongoose.Schema(
-  { name: String, roomNum: Number, login: Number },
+  { name: String, roomNum: Number, state: Number },
   options
 );
 // その形式のデータを保存・読み出しするために必要なモデルを作る
@@ -53,14 +53,14 @@ io.on("connection", (socket) => {
       const logName = await Name.find({
         roomNum: z,
         name: { $ne: name },
-        login: 1,
+        state: 1,
       });
       logName.forEach((p) => socket.emit("changeMember", p));
     }
     if (historyName) {
       socket.join(historyName.roomNum); //2回目以降の入室処理
       io.emit("changeMember", historyName); //名前送信時の処理
-      historyName.login = 1;
+      historyName.state = 1;
       socket.emit("roomNumSet", historyName.roomNum); //クライアント自身の画面にroomNumを表示させる
       let time = new Date();
       let timeGMT = time.getTime();
@@ -116,8 +116,8 @@ io.on("connection", (socket) => {
     });
     socket.on("disconnect", async () => {
       console.log(`${name} disconnected`);
-      const postData = await Name.findOne({ name: name });
-      postData.login = 0;
+      let postData = await Name.findOne({ name: name });
+      postData.state = 0;
       let num = postData.roomNum;
       let time = new Date();
       let timeGMT = time.getTime();
